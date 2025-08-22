bbcd387f81cc6c6241bfdf5d808eb1703a0e6833
eunae-cho <choeunae.off@gmail.com>
2025-08-22 11:29:55 +0900

    Add files via upload
    data-weather 값 변경 시, 
    
    1. 단순 스타일 변경 - CSS
    2. 동작 및 애니메이션 변경 - JS

--- script.js
+++ script.js
@@ -1,69 +1,48 @@
-const weather = ['sunny', 'snowy', 'rainy', 'cloudy'];
-
-// js로 요소 child 가지고와서 -> 1,2,3,4로 날씨 순서 정하기.
-document.body.dataset.weather = "sunny";
-var dataWeather = document.body.dataset.weather;
+// //전역변수 지역변수 
+const $body = document.querySelector('body');
+let dataWeather = $body.getAttribute('data-weather');
 
 const $picker = document.querySelector('#choose-weather>ul');
-const $background = document.querySelector(`#weather-is-${dataWeather} .background`);
+$picker.addEventListener("click", selectWeather);
+
+let $background;
 const $sun = document.querySelector(".sun");
 const $sunLight = document.querySelector(".sun-light");
 
-$picker.addEventListener("click", selectWeather);
 
-if(dataWeather=="sunny") {
-    $picker.children[0].style.background='none';
-    $picker.children[0].style.color='#000';
+function setBack() {
+    console.log("call::setBack");
 
-    document.querySelector("#weather-is-snowy").style.display = 'none';
-    document.querySelector("#weather-is-rainy").style.display = 'none';
-    document.querySelector("#weather-is-cloudy").style.display = 'none';
-    $background.style.background=`linear-gradient(90deg, #BCCCD1, #E8E1E1, #FFFFF2)`;
-    $background.addEventListener("mousemove", traceMouse);
-}
-else if(dataWeather=="snowy") {
-    document.querySelector("#weather-is-sunny").style.display = 'none';
-    document.querySelector("#weather-is-rainy").style.display = 'none';
-    document.querySelector("#weather-is-cloudy").style.display = 'none';
-    $background.style.background=`rgb(240, 255, 244)`;
-    $background.addEventListener("click", drawSnow);
-}
-else if(dataWeather=="rainy") {
-    document.querySelector("#weather-is-sunny").style.display = 'none';
-    document.querySelector("#weather-is-snowy").style.display = 'none';
-    document.querySelector("#weather-is-cloudy").style.display = 'none';
-
-    $background.style.background = "#6B2BF4";
-    $background.style.display = 'flex';
-    window.addEventListener("mousewheel", scrollBack);
-}
-else if(dataWeather=="cloudy") {
-    document.querySelector("#weather-is-sunny").style.display = 'none';
-    document.querySelector("#weather-is-snowy").style.display = 'none';
-    document.querySelector("#weather-is-rainy").style.display = 'none';
-    $background.style.background = "#3E88CC";
-    $background.addEventListener("click", drawRounds);
+    if(dataWeather==('sunny')) {
+        $background.addEventListener("mousemove", traceMouse);
+    }
+    else if(dataWeather=="snowy") {
+        $background.addEventListener("click", drawSnow);
+    }
+    else if(dataWeather==="rainy") {
+        window.addEventListener("mousewheel", scrollBack);
+    }
+    else if(dataWeather=="cloudy") {
+        $background.style.background = "#3E88CC";
+        $background.addEventListener("click", drawRounds);
+    }
 }
 
 function selectWeather(e) {
-    document.body.dataset.weather = e.target.id;
-    dataWeather = document.body.dataset.weather;
+    dataWeather = e.target.id;
+    $body.setAttribute('data-weather', e.target.id);
+    $background = document.querySelector(`#weather-is-${dataWeather} .background`);
 
-    console.log(dataWeather);
-
-    //weather값에 맞게 리렌더링
-    
+    setBack();
 
     //클릭시 다른 요소들은 회색 배경, 클릭한 것은 검은 컬러
-    [...$picker.children].forEach((child, i)=> {
-        console.log(i);
+    [...$picker.children].forEach((child)=> {
         child.style.background='rgba(182, 182, 182, 0.284)';
         child.style.color='#fff';
     })
+
     e.target.style.background='none';
     e.target.style.color='#000';
-
-    console.log(e);
 }
 
 
@@ -82,7 +61,7 @@ function drawSnow(e) {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     
-    console.log(mouseX, mouseY);
+    // console.log(mouseX, mouseY);
 
     const newStamp = document.createElement("div");
     newStamp.style.position = 'fixed';
